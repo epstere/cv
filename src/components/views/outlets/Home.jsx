d78f12857b55ed1062f74c13bbcbd2c22f6b2638
epstere <epstere@gmail.com>
2024-11-18 19:37:16 -0300

    Atualização

--- src/components/views/outlets/Home.jsx
+++ src/components/views/outlets/Home.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import styles from "./styles/Home.module.css"
+import React from "react";
+import styles from "./styles/Home.module.css";
 
 const Home = () => {
   return (
-    <div className={styles.main} >
+    <div className={styles.main}>
       <div className={styles.section_group}>
-        <div className={styles.section} >
+        <div className={styles.section}>
           <div className={styles.personalInfo}>
             <p className={styles.paragraph_header}>Informações Pessoais</p>
             <p className={styles.content_line}>Eliel Pereira Soares</p>
@@ -16,7 +16,7 @@ const Home = () => {
             <p className={styles.content_line}>Fazendinha, Araruama - RJ</p>
           </div>
         </div>
-        <div className={styles.section} >
+        <div className={styles.section}>
           <div className={styles.interests}>
             <p className={styles.paragraph_header}>Hobbies e Interesses</p>
             <p className={styles.content_line}>Ler</p>
@@ -26,15 +26,22 @@ const Home = () => {
           </div>
         </div>
       </div>
-      <div className={styles.section} >
+      <div className={styles.section}>
         <div className={styles.targets}>
           <p className={styles.paragraph_header}>Objetivos</p>
-          <p className={styles.content_line}> Como um profissional movido pela curiosidade incessante por novidades e dados históricos, meu objetivo é construir uma carreira de legado, marcada por feitos que possam ser lembrados e uma paixão por conhecimento infinito.<br />
-            Busco uma posição que me permita explorar constantemente novas fronteiras do conhecimento e contribuir de maneira significativa para o desenvolvimento da organização, sempre com o propósito de ser lembrado não apenas pelo impacto dos meus resultados, mas também pelo meu comprometimento contínuo com o aprendizado e a inovação.</p>
+          <p className={styles.content_line}>
+            Com 10+ anos de experiência, sou um profissional movido pela
+            curiosidade por novidades e pelo conteúdo histórico da empresa,
+            entendo que isso revela muito para onde se quer ir. Tenho paixão por
+            conhecimento infinito, estou sempre buscando explorar novas
+            fronteiras. Possuo excelente capacidade de comunicação e me
+            considero plural. Atualmente cursando pós-graduação em Direito
+            Digital na UVA.
+          </p>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- src/components/views/outlets/Professional.jsx
+++ src/components/views/outlets/Professional.jsx
@@ -1,49 +1,67 @@
-import React from 'react'
-import styles from "./styles/Professional.module.css"
+import React from "react";
+import styles from "./styles/Professional.module.css";
 
 const Professional = () => {
-    return (
-        <div className={styles.main} >
-            <div className={styles.section_group}>
-                <div className={styles.section} >
-                    <div className={styles.personalInfo}>
-                        <p className={styles.paragraph_header}>Projetos Relevantes</p>
-                        <p className={styles.content_line}><strong>www.deelite.com.br </strong> - Um site com área administrativa onde o proprietário altera as informações que serão exibidas. Apresenta-se como um portifólio de eventos esportivos.
-                        </p>
-                        <p className={styles.content_line}><strong>Confidencial </strong> - Protegido por sigilo de contrato até a sua publicação oficial, é um complexo sistema de venda online voltado para o segmento esportivo e de eventos
-                        </p>
-                    </div>
-                </div>
-                <div className={styles.section} >
-                    <div className={styles.personalInfo}>
-                        <p className={styles.paragraph_header}>Conhecimentos Técnicos</p>
-                        <p className={styles.content_line}>ReactJs, Node, Express</p>
-                        <p className={styles.content_line}>Javascript</p>
-                        <p className={styles.content_line}>Mongo, SQL</p>
-                        <p className={styles.content_line}>HTML, CSS</p>
-                        <p className={styles.content_line}>Git, Github</p>
-                        <p className={styles.content_line}>Deployment</p>
-                        <p className={styles.content_line}>API</p>
-                    </div>
-                </div>
-            </div>
-            <div className={styles.section}  >
-                <div className={styles.personalInfo}>
-                    <p className={styles.paragraph_header}>Experiência Profisional</p>
-                    <p className={styles.content_line}><strong>Militar do Exército Brasileiro (2012 - 2022): </strong>
-                        Além das atividades triviais da vida aquartelada, exercia a função de cabo aux de comunicações, na divisão de informática de minha Unidade. Lançava fios e cabos de fibra, planejava e estruturava redes locais de menor complexidade. Além disso, executava manutenção diária dos mais de 650 computadores que tínhamos sob nossa responsabilidade.
-                    </p>
-                    <p className={styles.content_line}><strong>Recepcionista em Hotel Royal Regency (2022-2023): </strong>
-                        Especial familiaridade com sistemas de gerenciamento, como PC-System e Facility, além de uma excelente capacidade de comunicação.
-                    </p>
-                    <p className={styles.content_line}><strong>Programador Freelancer (2024): </strong>
-                        Embora esteja entrando calmamente no mercado, tenho dois projetos publicados, sendo um deles um complexo sistema de venda online.
-                    </p>
-
-                </div>
-            </div>
+  return (
+    <div className={styles.main}>
+      <div className={styles.section_group}>
+        <div className={styles.section}>
+          <div className={styles.personalInfo}>
+            <p className={styles.paragraph_header}>Projetos Relevantes</p>
+            <p className={styles.content_line}>
+              <strong>www.deelite.com.br </strong> - Um site que permite que o
+              gestor altere as informações que serão exibidas a cada evento que
+              tenha interesse em divulgar.
+            </p>
+            <p className={styles.content_line}>
+              <strong>Confidencial </strong> - atualmente em fase de
+              finalização, no desenvolvimento de um complexo sistema de venda
+              online voltado para o segmento esportivo e de eventos. Essa
+              aplicação é demasiadamente sofisticada, porque foi criada uma
+              solução própria de comunicação online com o banco, sem
+              intermediários. Nesse momento ainda protegido por sigilo de
+              contrato até a sua publicação oficial.
+            </p>
+          </div>
         </div>
-    )
-}
+        <div className={styles.section}>
+          <div className={styles.personalInfo}>
+            <p className={styles.paragraph_header}>Conhecimentos Técnicos</p>
+            <p className={styles.content_line}>ReactJs, Node, Express</p>
+            <p className={styles.content_line}>Javascript</p>
+            <p className={styles.content_line}>Mongo, SQL</p>
+            <p className={styles.content_line}>HTML, CSS</p>
+            <p className={styles.content_line}>Git, Github</p>
+            <p className={styles.content_line}>Deployment</p>
+            <p className={styles.content_line}>API</p>
+          </div>
+        </div>
+      </div>
+      <div className={styles.section}>
+        <div className={styles.personalInfo}>
+          <p className={styles.paragraph_header}>Experiência Profissional</p>
+          <p className={styles.content_line}>
+            <strong>Militar do Exército Brasileiro (2012 - 2022): </strong>
+            Embora não tenha muito projetos grandes, mas me sinto entrando no
+            mercado de programação. Possuo dois projetos publicados, sendo um
+            deles de um complexo sistema de venda online.
+          </p>
+          <p className={styles.content_line}>
+            <strong>Recepcionista em Hotel Royal Regency (2022-2023): </strong>
+            Atendimento ao cliente no checkin, checkout e outras demandas. Com
+            esta experiência criei uma familiaridade com os sistemas de
+            gerenciamento, tais como; PC-System e Facility.
+          </p>
+          <p className={styles.content_line}>
+            <strong>Programador Freelancer (2024): </strong>
+            Embora não tenha muito projetos grandes, mas me sinto entrando no
+            mercado de programação. Possuo dois projetos publicados, sendo um
+            deles de um complexo sistema de venda online.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Professional
+export default Professional;
